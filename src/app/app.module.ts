d45bd669f1a5b2eafbdcfeabeaa12c179f3b2d30
kirio93 <manuel.ferrucci93@gmail.com>
2017-12-12 15:08:48 +0100

    tutto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,45 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './login/login.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import {MatButtonModule, MatCheckboxModule, MatMenuModule, MatToolbarModule} from '@angular/material';
+import { AppRoutingModule } from './/app-routing.module';
+import { RegisterComponent } from './register/register.component';
+import { ListProductComponent } from './list-product/list-product.component';
+import {LoginService} from "./Providers/login.service";
+import {InterceptorService} from "./Providers/interceptor.service";
+import {AuthGuardService} from "./Providers/auth-guard.service";
+import {SharedService} from "./Providers/shared.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ListProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    MatButtonModule, MatCheckboxModule,
+    MatToolbarModule,
+    MatMenuModule,
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [LoginService,
+
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: InterceptorService,
+      multi:true,
+    },
+    AuthGuardService,
+    SharedService,],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
