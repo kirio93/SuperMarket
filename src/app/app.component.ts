d891d4626cbba3a9f2cd3551ef41ad45666320bf
kirio93 <manuel.ferrucci93@gmail.com>
2017-12-12 15:29:51 +0100

    logout e base 64

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {LoginService} from "./Providers/login.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  logged= false;
+
+constructor (private loginService:LoginService, private router:Router,){
+  let userLogger=localStorage.getItem('user');
+  if(userLogger!= null)
+    this.logged=true;
+  let token: string[]= atob(localStorage.getItem('token')).split(':');
+  let user= {username:token[0], password:token[1]};
+  this.loginService.login(user).subscribe(data=>
+  {console.log('logged'+ data);
+    localStorage.setItem('user', JSON.stringify(data));
+    localStorage.setItem('token', btoa(user.username+':'+ user.password));
+    this.logged= true;
+    this.router.navigate(['list-product']);
+  }, (err)=>{
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
+    this.logged=true;
+    this.router.navigate(['login'])
+  })
+}
+  logout(){
+    this.loginService.logout().subscribe( data=>{
+      console.log('logged out.'+data);
+      localStorage.removeItem('user');
+      localStorage.removeItem('token');
+      this.router.navigate(['login']);
+    }, (err)=>{
+      console.log('logger out. ')
+      localStorage.removeItem('user');
+      this.router.navigate(['login']);
+      this.logged=false
+    })
+  }
+
+
+
+
 }
