b7a41964c765bdf8e93d8f7ae425bc659da6dda6
a-storti <antonello.storti@gmail.com>
2017-12-19 13:00:32 +0100

    sconti del giorno

--- src/app/list-product/list-product.component.ts
+++ src/app/list-product/list-product.component.ts
@@ -4,8 +4,6 @@ import {ListProductService} from '../providers/list-product.service';
 import {Location} from '@angular/common';
 
 
-
-
 @Component({
   selector: 'app-list-product',
   templateUrl: './list-product.component.html',
@@ -13,10 +11,12 @@ import {Location} from '@angular/common';
 })
 export class ListProductComponent implements OnInit {
 
-  listProdotti: Array<Prodotto>= [];
-  carrello: Array<Prodotto>=[];
+  listProdotti: Array<Prodotto> = [];
+  carrello: Array<Prodotto> = [];
   prodotto: Prodotto = new Prodotto();
-  listaStorico:Array<Prodotto>= [];
+  listaStorico: Array<Prodotto> = [];
+  listOfferta: Array<Prodotto>=[];
+
 
 
   constructor(private prodottiService: ListProductService,
@@ -25,42 +25,45 @@ export class ListProductComponent implements OnInit {
 
   ngOnInit() {
     this.getall();
+
   }
 
 
   getall() {
     this.prodottiService.getall().subscribe(data => {
       this.listProdotti = data;
-      console.log("getall")
+      console.log('getall');
+      this.offerteDelGiorno();
+    });
+
+  }
+
+  selectProdotto(prodotto: Prodotto) {
+    this.prodotto = prodotto;
+  }
+
+
+  getListDisponibili() {
+    this.prodottiService.getListDisponibili().subscribe(data => {
+      this.listProdotti = data;
     });
   }
 
-  selectProdotto(prodotto :Prodotto){
-      this.prodotto=prodotto;
+  findStorico() {
+    this.prodottiService.findStorico().subscribe(data => {
+      this.listaStorico = data;
+    });
   }
 
-
-  getListDisponibili (){
-    this.prodottiService.getListDisponibili().subscribe(data=>{
-      this.listProdotti=data;
-    })
-  }
-
-  findStorico(){
-    this.prodottiService.findStorico().subscribe(data=>{
-      this.listaStorico=data;
-    })
-  }
-
-  deleteProdotto(idProdotto){
+  deleteProdotto(idProdotto) {
     this.prodottiService.deleteProdotto(idProdotto);
 
   }
 
-  listaDisponibiliCategoria(categoria, disponibili){
-    this.prodottiService.categoriaDisponibili(categoria, disponibili).subscribe(data=>{
-      this.listProdotti=data;
-    })
+  listaDisponibiliCategoria(categoria, disponibili) {
+    this.prodottiService.categoriaDisponibili(categoria, disponibili).subscribe(data => {
+      this.listProdotti = data;
+    });
   }
 
   aggiungi(prodotto: Prodotto) {
@@ -68,7 +71,19 @@ export class ListProductComponent implements OnInit {
     this.prodottiService.modificaProdotto(prodotto);
     console.log(prodotto);
     this.carrello.push(prodotto);
-    localStorage.setItem("carrello", JSON.stringify(this.carrello));
-    console.log("carrello: "+ localStorage.getItem("carrello").toString());
+    localStorage.setItem('carrello', JSON.stringify(this.carrello));
+    console.log('carrello: ' + localStorage.getItem('carrello').toString());
+  }
+
+  offerteDelGiorno() {
+    for (var _i = 0; _i < 5; _i++) {
+      let casuale: number = Math.round((Math.random() * this.listProdotti.length));
+      let sconto = this.listProdotti[casuale].prezzoUnitario - (this.listProdotti[casuale].prezzoUnitario * 0.2);
+      let decimal: number= Number(parseFloat(sconto.toString()).toFixed(2));
+      this.listProdotti[casuale].prezzoUnitario=decimal;
+      this.listOfferta.push(this.listProdotti[casuale]);
+      this.listProdotti[casuale].offerta=1;
+      console.log(this.listProdotti[casuale].prezzoUnitario);
+    }
   }
 }
